d17ce9cbcaa53957b7fcf65bbc8eb02a3986aebc
Pantera <w.jung@okestro.com>
2024-06-04 21:16:12 +0900

    refactor: rename classes

--- src/main/java/com/github/attacktive/msaorder/order/adapter/outbound/OrderAdapter.java
+++ src/main/java/com/github/attacktive/msaorder/order/adapter/outbound/OrderAdapter.java
@@ -4,30 +4,30 @@ import java.util.List;
 import java.util.Optional;
 
 import com.github.attacktive.msaorder.order.adapter.outbound.persistence.OrderEntity;
-import com.github.attacktive.msaorder.order.adapter.outbound.persistence.OrderJpaRepository;
+import com.github.attacktive.msaorder.order.adapter.outbound.persistence.OrderRepository;
 import com.github.attacktive.msaorder.order.application.request.OrderRequest;
 import com.github.attacktive.msaorder.order.domain.Order;
-import com.github.attacktive.msaorder.order.port.outbound.OrderRepository;
+import com.github.attacktive.msaorder.order.port.outbound.OrderPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
-public class OrderRepositoryImpl implements OrderRepository {
-	private final OrderJpaRepository orderJpaRepository;
+public class OrderAdapter implements OrderPort {
+	private final OrderRepository orderRepository;
 
 	@Override
 	@Transactional(readOnly = true)
 	public Optional<Order> findById(long id) {
-		return orderJpaRepository.findById(id)
+		return orderRepository.findById(id)
 			.map(OrderEntity::toOrder);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Order> findAll() {
-		return orderJpaRepository.findAll()
+		return orderRepository.findAll()
 			.stream()
 			.map(OrderEntity::toOrder)
 			.toList();
@@ -36,13 +36,13 @@ public class OrderRepositoryImpl implements OrderRepository {
 	@Override
 	@Transactional
 	public Order save(OrderRequest orderRequest) {
-		return orderJpaRepository.save(new OrderEntity(orderRequest))
+		return orderRepository.save(new OrderEntity(orderRequest))
 			.toOrder();
 	}
 
 	@Override
 	@Transactional
 	public void deleteById(long id) {
-		orderJpaRepository.deleteById(id);
+		orderRepository.deleteById(id);
 	}
 }
--- src/main/java/com/github/attacktive/msaorder/order/adapter/outbound/persistence/OrderRepository.java
+++ src/main/java/com/github/attacktive/msaorder/order/adapter/outbound/persistence/OrderRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> { }
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> { }
