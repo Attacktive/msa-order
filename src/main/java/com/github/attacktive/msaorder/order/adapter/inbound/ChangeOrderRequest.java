4cc41549403e60190c6867d617db6d72648788cb
Attacktive <tribute2pantera@gmail.com>
2024-06-07 15:46:32 +0900

    fix: correct wrong validation rules for ordering quantity
    Ordering zero product doesn't make sense, eh?

--- src/main/java/com/github/attacktive/msaorder/order/adapter/inbound/ChangeOrderRequest.java
+++ src/main/java/com/github/attacktive/msaorder/order/adapter/inbound/ChangeOrderRequest.java
@@ -1,9 +1,9 @@
 package com.github.attacktive.msaorder.order.adapter.inbound;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Positive;
 
-public record ChangeOrderRequest(Long id, @NotNull Long productId, @NotNull @PositiveOrZero Long quantity) implements OrderRequest {
+public record ChangeOrderRequest(Long id, @NotNull Long productId, @NotNull @Positive Long quantity) implements OrderRequest {
 	public ChangeOrderRequest withId(Long id) {
 		return new ChangeOrderRequest(id, productId, quantity);
 	}
