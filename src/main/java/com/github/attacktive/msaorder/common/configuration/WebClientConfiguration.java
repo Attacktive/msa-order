34286bc35c5f25535760d418e28c899b2f70162b
Attacktive <tribute2pantera@gmail.com>
2024-06-08 13:47:00 +0900

    test: fix broken mocking setup
    https://github.com/Attacktive/msa-order/issues/8

--- src/main/java/com/github/attacktive/msaorder/common/configuration/WebClientConfiguration.java
+++ src/main/java/com/github/attacktive/msaorder/common/configuration/WebClientConfiguration.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
+import org.springframework.web.reactive.function.client.ExchangeFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -16,7 +19,13 @@ public class WebClientConfiguration {
 		return WebClient.builder()
 			.baseUrl(appProperties.getBaseUrlToProducts())
 			.defaultHeaders(httpHeaders -> httpHeaders.setContentType(MediaType.APPLICATION_JSON))
+			.exchangeFunction(exchangeFunction())
 			.codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(-1))
 			.build();
+	}
+
+	@Bean
+	public ExchangeFunction exchangeFunction() {
+		return ExchangeFunctions.create(new ReactorClientHttpConnector());
 	}
 }
