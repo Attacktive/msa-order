7d7ced510dbb11111f0d772e1b247498fbbddbaa
Attacktive <tribute2pantera@gmail.com>
2024-06-08 12:38:44 +0900

    feature: handle Orders having product that does not exist anymore

--- src/main/java/com/github/attacktive/msaorder/order/adapter/outbound/OrderResponse.java
+++ src/main/java/com/github/attacktive/msaorder/order/adapter/outbound/OrderResponse.java
@@ -2,4 +2,8 @@ package com.github.attacktive.msaorder.order.adapter.outbound;
 
 import com.github.attacktive.msaorder.order.domain.Product;
 
-public record OrderResponse(Long id, Product product, Long quantity) { }
+public record OrderResponse(Long id, Product product, Long quantity) {
+	public static OrderResponse ofNonExistentProduct(Long id, Long productId, Long quantity) {
+		return new OrderResponse(id, Product.nonExistent(productId), quantity);
+	}
+}
