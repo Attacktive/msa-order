21f4691fc5f89a49ab70244af21c0bca8711d26b
Attacktive <tribute2pantera@gmail.com>
2024-06-08 14:34:08 +0900

    test: add tests for `OrderUseCase` methods
    https://github.com/Attacktive/msa-order/issues/8

--- src/test/java/com/github/attacktive/msaorder/order/port/inbound/OrderUseCaseTest.java
+++ src/test/java/com/github/attacktive/msaorder/order/port/inbound/OrderUseCaseTest.java
@@ -1,10 +1,14 @@
 package com.github.attacktive.msaorder.order.port.inbound;
 
+import java.util.Collections;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.attacktive.msaorder.order.adapter.inbound.ChangeOrderRequest;
 import com.github.attacktive.msaorder.order.adapter.inbound.OrderProductRequest;
 import com.github.attacktive.msaorder.order.adapter.outbound.persistence.OrderRepository;
 import com.github.attacktive.msaorder.order.domain.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,13 +50,125 @@ class OrderUseCaseTest {
 	}
 
 	@Test
-	@DisplayName("orderProduct")
-	void testOrderProduct() {
-		var product = new Product(1L, "product", "product description", 1000L, 10L);
-		mockExchangeFunction(product);
+	@DisplayName("getOrders")
+	void testGetOrders() {
+		var mockProduct = new Product(1L, "product", "product description", 1000L, 10L);
+		mockExchangeFunction(mockProduct);
 
 		var orderProductRequest = new OrderProductRequest(1L, 1L);
 		orderUseCase.orderProduct(orderProductRequest);
+
+		var mockProducts = Collections.singletonList(mockProduct);
+		mockExchangeFunction(mockProducts);
+
+		var orders = orderUseCase.getOrders();
+		Assertions.assertEquals(1, orders.size());
+
+		var order = orders.get(0);
+		Assertions.assertEquals(1, order.quantity());
+
+		var orderedProduct = order.product();
+		Assertions.assertEquals(1, orderedProduct.id());
+		Assertions.assertEquals("product", orderedProduct.name());
+		Assertions.assertEquals("product description", orderedProduct.description());
+		Assertions.assertEquals(1000, orderedProduct.price());
+		Assertions.assertEquals(10, orderedProduct.quantity());
+	}
+
+	@Test
+	@DisplayName("orderExistsHavingProduct")
+	void testOrderExistsHavingProduct() {
+		var mockProduct = new Product(1L, "product", "product description", 1000L, 10L);
+		mockExchangeFunction(mockProduct);
+
+		var orderProductRequest = new OrderProductRequest(1L, 1L);
+		orderUseCase.orderProduct(orderProductRequest);
+
+		var orderExists = orderUseCase.orderExistsHavingProduct(1);
+
+		Assertions.assertTrue(orderExists);
+	}
+
+	@Test
+	@DisplayName("getOrder")
+	void testGetOrder() {
+		var mockProduct = new Product(1L, "product", "product description", 1000L, 10L);
+		mockExchangeFunction(mockProduct);
+
+		var orderProductRequest = new OrderProductRequest(1L, 1L);
+		var orderId = orderUseCase.orderProduct(orderProductRequest).id();
+
+		var order = orderUseCase.getOrder(orderId);
+		Assertions.assertEquals(1, order.quantity());
+
+		var orderedProduct = order.product();
+		Assertions.assertEquals(1, orderedProduct.id());
+		Assertions.assertEquals("product", orderedProduct.name());
+		Assertions.assertEquals("product description", orderedProduct.description());
+		Assertions.assertEquals(1000, orderedProduct.price());
+		Assertions.assertEquals(10, orderedProduct.quantity());
+	}
+
+	@Test
+	@DisplayName("changeOrder: the quantity")
+	void testChangeOrderQuantity() {
+		var mockProduct = new Product(1L, "product", "product description", 1000L, 10L);
+		mockExchangeFunction(mockProduct);
+
+		var orderProductRequest = new OrderProductRequest(1L, 1L);
+		var order = orderUseCase.orderProduct(orderProductRequest);
+		var orderId = order.id();
+		var product = order.product();
+
+		var changeOrderRequest = new ChangeOrderRequest(orderId, 1L, 2L);
+		var changedOrder = orderUseCase.changeOrder(orderId, changeOrderRequest);
+		Assertions.assertEquals(orderId, changedOrder.id());
+		Assertions.assertEquals(2, changedOrder.quantity());
+
+		var changedProduct = changedOrder.product();
+		Assertions.assertEquals(product.id(), changedProduct.id());
+
+		/*
+		 * The following test fails ☠ because the response is mocked.
+		 * I wouldn't change the mock response and test since that's pointless.
+		 */
+		//Assertions.assertEquals(product.quantity() - 1, changedProduct.quantity());
+
+		Assertions.assertTrue(orderRepository.findById(orderId).isPresent());
+		Assertions.assertEquals(1, orderRepository.count());
+	}
+
+	@Test
+	@DisplayName("deleteOrder")
+	void testDeleteOrder() {
+		var mockProduct = new Product(1L, "product", "product description", 1000L, 10L);
+		mockExchangeFunction(mockProduct);
+
+		var orderProductRequest = new OrderProductRequest(1L, 1L);
+		var orderId = orderUseCase.orderProduct(orderProductRequest).id();
+
+		orderUseCase.deleteOrder(orderId);
+		Assertions.assertTrue(orderRepository.findById(orderId).isEmpty());
+		Assertions.assertEquals(0, orderRepository.count());
+	}
+
+	@Test
+	@DisplayName("orderProduct")
+	void testOrderProduct() {
+		var mockProduct = new Product(1L, "product", "product description", 1000L, 10L);
+		mockExchangeFunction(mockProduct);
+
+		var orderProductRequest = new OrderProductRequest(1L, 1L);
+		var orderResponse = orderUseCase.orderProduct(orderProductRequest);
+		var orderedProduct = orderResponse.product();
+
+		Assertions.assertEquals(1, orderedProduct.id());
+		Assertions.assertEquals("product", orderedProduct.name());
+		Assertions.assertEquals("product description", orderedProduct.description());
+		Assertions.assertEquals(1000, orderedProduct.price());
+		Assertions.assertEquals(10, orderedProduct.quantity());
+
+		Assertions.assertEquals(1, orderRepository.count());
 	}
 
 	private void mockExchangeFunction(Object desiredResponse) {
